78b64e1d1454a1be515aa601e75c29874177bead
Ryan Veitch <ryan.r.veitch@gmail.com>
2015-09-27 17:24:55 -0500

    Update volte-face-midi.ino

--- volte-face-midi.ino
+++ volte-face-midi.ino
@@ -1,61 +1,26 @@
-
-/*! Running status enables short messages when sending multiple values
-     of the same type and channel.\n
-     Set to 0 if you have troubles controlling your hardware.
-     //static const bool UseRunningStatus = true;
-     */
-
-/*!
-MIDI.begin();   // Launch MIDI with default options
-MIDI.read(); //is there incoming MIDI?
-getType(): returns the type of the message (valid types are decribed above)
-getChannel(): returns the channel of the message (1-16)
-getData1(): returns the first data byte (0-127)
-getData2(): returns the 2nd data byte (0-127)
-check(): returns true if the message is valid, else false.
-MIDI.turnThruOn();
-MIDI.turnThruOff();
-MIDI.setInputChannel(1); // input channel is set to 1
-UseRunningStatus = true
-  //Running status enables short messages when sending multiple values of the same type and channel.
-  //Set to 0 if you have troubles controlling your hardware.
-MIDI.getData1();
-getType();
-getTypeFromStatusByte();
-send();
-setHandleProgramChange();
-setThruFilterMode();
-disconnectCallbackFromType();
-     */
-// -----------------------------------------------------------------------------
-     
 #include <MIDI.h>
-//Create an instance of the library with default name, serial port and settings
-//MIDI_CREATE_DEFAULT_INSTANCE();
-MIDI_CREATE_INSTANCE(HardwareSerial, Serial, MIDI);
+MIDI_CREATE_DEFAULT_INSTANCE();
 
-int ledPin = 13;                 // LED connected to digital pin 13
-byte P = 0;  //program or patch number
-
-// -----------------------------------------------------------------------------
-// This function will be automatically called when a PC is received.
-// It must be a void-returning function with the correct parameters,
+// --------------------------------------------------------------------------------------------------
+// Program Change Callback:
+// This function will be automatically called when a program change message (0xC0) has been received.
 void handleProgramChange(byte channel, byte number)
 {
-    // Do whatever you want when a pc is pressed.
-    // Try to keep your callbacks short (no delays ect)
-    // otherwise it would slow down the loop() and have a bad impact
-    // on real-time performance.
-    MIDI.sendControlChange(0, 0, 1);
+  // Send Control Change (Bank Select, MSB)
+  MIDI.sendControlChange(0, 0, 1); // Needed for Program Changes to work
 }
-     
-void setup() {
-  pinMode(ledPin, OUTPUT);      // sets the digital pin as output
-  MIDI.setHandleProgramChange(handleProgramChange);
-  MIDI.begin();
+// --------------------------------------------------------------------------------------------------
+
+void setup()
+{
+  // Connect the "handleProgramChange" function to the library for program change callback reception.
+  MIDI.handleProgramChange(handleProgramChange);
+  // Initiate MIDI communications, listen to all channels
+  MIDI.begin(MIDI_CHANNEL_OMNI); 
 }
 
-void loop() {
+void loop()
+{
   MIDI.read(); //is there incoming MIDI?
   //{
     //for (int pgm = 1; pgm < 99; pgm++) // Sets up for the voice loop
