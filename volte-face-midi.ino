7e938e147629cf9525d46e13a088e5edd76d6004
Ryan Veitch <ryan.r.veitch@gmail.com>
2015-09-27 17:26:10 -0500

    Update volte-face-midi.ino

--- volte-face-midi.ino
+++ volte-face-midi.ino
@@ -25,13 +25,9 @@ void loop()
   //{
     //for (int pgm = 1; pgm < 99; pgm++) // Sets up for the voice loop
     //{
-      //digitalWrite(ledPin, HIGH);   // sets the LED on
       //MIDI.sendProgramChange(pgm, 1); // Change the voice
-      //MIDI.sendNoteOff(42,0,1);   // Stop the note
-      //MIDI.sendControlChange(0, 0, 1);
-      //delay(1000);      // 1 second delay
-      //digitalWrite(ledPin, LOW);    // sets the LED off
-      //delay(1000);                  // waits for a second
+      //MIDI.sendControlChange(0, 0, 1); // Needed for Program Changes to work
+      //delay(1000); // 1 second delay
     //}
   //}
 }
