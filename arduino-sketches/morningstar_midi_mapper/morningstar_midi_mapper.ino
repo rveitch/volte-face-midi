6f68cc2d5804a4e3e7fb54b80e663b92c653c567
ryan.veitch <ryan.r.veitch@gmail.com>
2020-11-17 17:03:25 -0600

    Morningstar Midi Mapper v1.0

--- arduino-sketches/morningstar_midi_mapper/morningstar_midi_mapper.ino
+++ arduino-sketches/morningstar_midi_mapper/morningstar_midi_mapper.ino
@@ -9,11 +9,11 @@ LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
 // Create an instance of the library with default name, serial port and settings
 MIDI_CREATE_DEFAULT_INSTANCE();
 
-#define MIDI_THRU_DISABLED false
+#define MIDI_THRU_DISABLED true
 #define MIDI_CHANNEL_LISTEN MIDI_CHANNEL_OMNI // listen to all channels
-// #define MIDI_CHANNEL_OUT 1
+#define MIDI_CHANNEL_OUT 1
 
-#define MIDI_OFFSET 1
+#define MIDI_OFFSET 0
 #define CONTROL_CHANGE_VALUE 1
 #define PRESETS_PER_BANK 12
 #define PRESET_MIN 10
@@ -23,32 +23,39 @@ void setup() {
 
   MIDI.setHandleError(handleError);
   MIDI.setHandleProgramChange(handleProgramChange);
-  MIDI.setHandleControlChange(handleControlChange);
+  // MIDI.setHandleControlChange(handleControlChange);
 
-  if (MIDI_THRU_DISABLED) {
-    MIDI.turnThruOff();
-  }
+  //if (MIDI_THRU_DISABLED == true) {
+    //MIDI.turnThruOff();
+  //}
 
   MIDI.begin(MIDI_CHANNEL_LISTEN); // Initiate MIDI communications
+
+  MIDI.turnThruOff();
+  // MIDI.setThruFilterMode(DifferentChannel); // All the messages but the ones on the Input Channel will be sent back.
+
+  resetMorningstarMidi();
+  displayReadyState();
 }
 
 void loop() {
-  //MIDI.read();
+  MIDI.read();
 }
 
 void bootDisplay() {
   // Set up the LCD's number of rows and columns:
   lcd.begin(16, 2);
-
+  
   // Home message
   lcd.setCursor(0, 0);
   lcd.print(" Veitch Digital ");
   lcd.setCursor(0, 1);
   lcd.print("  MIDI Devices  ");
 
-  delay (3000);
+  //delay (3000);
+}
 
-  // Ready State
+void displayReadyState() {
   lcd.setCursor(0, 0);
   lcd.print(" AWAITING  MIDI ");
   lcd.setCursor(0, 1);
@@ -63,11 +70,18 @@ void resetDisplay() {
 void resetProgramChangeDisplay() {
   lcd.setCursor(0, 0);
   lcd.print(" IN: PC    Ch   ");
+  lcd.setCursor(0, 1);
+  lcd.print("OUT: PC    CC   ");
 }
 
 void resetControlChangeDisplay() {
   lcd.setCursor(0, 1);
   lcd.print("OUT: PC    CC   ");
+}
+
+void resetMorningstarMidi() {
+  MIDI.sendProgramChange(0, MIDI_CHANNEL_OUT);
+  MIDI.sendControlChange(10, CONTROL_CHANGE_VALUE, MIDI_CHANNEL_OUT);
 }
 
 void handleControlChange(byte channel, byte number, byte value) {
@@ -87,19 +101,19 @@ void handleProgramChange(byte channel, byte incomingProgramChange) {
   resetProgramChangeDisplay();
 
   // DISPLAY the PC IN Number
-  lcd.setCursor(8, 0);
+  lcd.setCursor(7,0);
   lcd.print(incomingProgramChange, DEC);
 
   // DISPLAY the CHANNEL
-  lcd.setCursor(15, 0);
+  lcd.setCursor(15,0);
   lcd.print(channel, DEC);
 
   // DISPLAY the PC OUT Number
-  lcd.setCursor(8, 1);
+  lcd.setCursor(7,1);
   lcd.print(bankPC, DEC);
 
   // DISPLAY the CC OUT Number
-  lcd.setCursor(14, 1);
+  lcd.setCursor(14,1);
   lcd.print(presetCC, DEC);
 }
 
